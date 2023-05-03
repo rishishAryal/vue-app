341105d8934b54a05d16b12e61f674fa995f8337
rishisAryal <risharyal5@gmail.com>
2023-05-03 19:05:03 +0545

    made new components

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>Home Components </h1>
+<h1>Email is {{ email }} </h1>
+<h1>email is {{ getData().email }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data(){
+        return {
+            email:'risharyal5@gmail.com'
+        }
+    },
+    methods:{
+        getName(){
+            return "Rishish Aryal"
+        },
+        getData(){
+            return  {
+                name:"rish",
+                age:20,
+                email:this.email
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+h1{
+    color: rgb(9, 124, 85);
+}
+</style>
